01b040bf1aa96a6ae751a015b0c53d7f8f860818
Corbin Jensen <corbinjensen@users.noreply.github.com>
2021-10-07 00:47:19 -0700

    First Commit, I hope that it sees the ignore file

---
+++ app/src/test/java/com/company/cmpt276_asn3/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package com.company.cmpt276_asn3;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Example local unit test, which will execute on the development machine (host).
+ *
+ * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ */
+public class ExampleUnitTest {
+    @Test
+    public void addition_isCorrect() {
+        assertEquals(4, 2 + 2);
+    }
+}
