74fe7fda603bb394025e06a4cd098f23e80246f5
Johnny <johnnymai951@gmail.com>
2021-10-15 01:43:23 -0700

    Create stub functions for GameField

--- app/src/main/java/com/company/cmpt276_asn3/model/GameField.java
+++ app/src/main/java/com/company/cmpt276_asn3/model/GameField.java
@@ -6,13 +6,33 @@ import java.util.List;
 // Holds underlying 2D array representing the playable game field
 public class GameField {
     // 2D array of cells in row major order
-    // field[0] is first row, etc
     private List<List<Cell>> field = new ArrayList<>();
+    private int numRows;
+    private int numCols;
 
-    // Life cycle follows:
-    // Make game field -> populate array with cell objects -> randomly set mines -> update numMines
-    // UI queries game obj about cell status
-    // Update numMines as game progresses
+    // Initialize 2D array here
+    public GameField(int height, int width){
+        this.numRows = height;
+        this.numCols = width;
 
-    // May need separate interface class to interact with game field?
+        populateListWithCells();
+    }
+
+    public Cell getCell(int row, int col){
+        return field.get(row).get(col);
+    }
+
+    private void populateListWithCells(){
+
+    }
+
+    private void setMines(){
+
+    }
+
+    private void updateNumMines(){
+
+    }
+
+
 }
