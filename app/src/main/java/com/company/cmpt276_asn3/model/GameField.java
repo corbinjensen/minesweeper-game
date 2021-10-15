4aa75bcef5159422e41754668a8934a6dcfff989
Johnny <johnnymai951@gmail.com>
2021-10-15 01:59:31 -0700

    Add functions to manage game field 2D array
    Create functions to add cells and count mines

--- app/src/main/java/com/company/cmpt276_asn3/model/GameField.java
+++ app/src/main/java/com/company/cmpt276_asn3/model/GameField.java
@@ -16,6 +16,8 @@ public class GameField {
         this.numCols = width;
 
         populateListWithCells();
+        setMines();
+        updateNumMines();
     }
 
     public Cell getCell(int row, int col){
@@ -23,16 +25,58 @@ public class GameField {
     }
 
     private void populateListWithCells(){
+        for (int i = 0; i < numRows; i++){
+            List<Cell> row = new ArrayList<>();
 
+            for (int j = 0; i < numCols; j++){
+                // Blank cell has no mine and no count
+                Cell c = new Cell(false, false, 0);
+                row.add(c);
+            }
+            field.add(row);
+        }
     }
 
     private void setMines(){
-
+        // Randomly sets mines in game field
     }
 
     private void updateNumMines(){
 
+        for (int i = 0; i < numRows; i++){
+            int rowCount = countNumMinesInRow(i);
+
+            for (int j = 0; j < numCols; j++){
+                int colCount = countNumMinesInCol(j);
+                field.get(i).get(j).setNumMines(rowCount + colCount);
+            }
+        }
     }
 
+    private int countNumMinesInRow(int index){
+        int count = 0;
+        List<Cell> row = field.get(index);
 
+        // Count all mines that are not revealed
+        for (int i = 0; i < numCols; i++){
+            if (row.get(i).isContainsMine() && !row.get(i).isShown()){
+                count++;
+            }
+        }
+        return count;
+    }
+
+    private int countNumMinesInCol(int index){
+        int count = 0;
+
+        for (int i = 0; i < numRows; i++){
+            List<Cell> row = field.get(i);
+
+            if (row.get(index).isContainsMine() && !row.get(index).isShown()){
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
