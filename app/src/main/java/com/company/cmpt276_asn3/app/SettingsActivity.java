930125300c72a6150b14334d1147737552a54f19
Corbin Jensen <corbinjensen@users.noreply.github.com>
2021-10-16 17:02:49 -0700

    -Create Activities for Settings, and GamePlay

---
+++ app/src/main/java/com/company/cmpt276_asn3/app/SettingsActivity.java
@@ -0,0 +1,16 @@
+package com.company.cmpt276_asn3.app;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.company.cmpt276_asn3.R;
+
+public class SettingsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_settings);
+    }
+}
