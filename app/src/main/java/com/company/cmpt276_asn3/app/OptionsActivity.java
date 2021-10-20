1074fcf4d39345654d3f81a0ecb265fa7f6c559f
Johnny <johnnymai951@gmail.com>
2021-10-19 20:27:13 -0700

    import stuff

--- app/src/main/java/com/company/cmpt276_asn3/app/OptionsActivity.java
+++ app/src/main/java/com/company/cmpt276_asn3/app/OptionsActivity.java
@@ -9,6 +9,8 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
 
